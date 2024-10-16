c4f151cbc6be8825e92f563ac4657b0a537a27c2
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-10-16 20:54:07 +0100

    project created

---
+++ gyms/src/main/java/deti/fitmonitor/gyms/GymsApplication.java
@@ -0,0 +1,13 @@
+package deti.fitmonitor.gyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymsApplication.class, args);
+	}
+
+}
